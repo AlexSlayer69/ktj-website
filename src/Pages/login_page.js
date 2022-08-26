350aae03ac267da21c33a355dae38f6bc79f8aac
AlexSlayer69 <akulvjha2808@gmail.com>
2022-08-27 02:48:22 +0530

    Log In Page components

--- src/Pages/login_page.js
+++ src/Pages/login_page.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import {Box} from '../components/site_elements'
+import {Box,Txt,Pass,Btn} from '../components/site_elements'
+import styled from 'styled-components';
+
+export const Cont = styled.div`
+display: grid;
+grid-template-columns: 1fr 1fr;
+grid-template-rows: 1fr;
+align-items: center;
+gap: 1em;
+justify-content: center;
+`;
 
 const Login = () => {
   return (
+    <Cont>
       <Box>
-        addf
+        <h1 style={{gridColumn: "1/-1",color:"White"}} > Welcome Back! </h1>
+        <p style={{color:"white"}}>Username</p><Txt/>
+        <p style={{color:"white"}}>Password </p><Pass/>
+        <Btn>Log In</Btn>
       </Box>
+    </Cont>
   );
 };
   
--- src/components/site_elements.js
+++ src/components/site_elements.js
@@ -1,10 +1,42 @@
 import styled from "styled-components";
 
 export const Box = styled.div`
-display: flex;
+display: grid;
+grid-template-columns: 1fr 1fr;
+grid-template-rows: 1fr 1fr 1fr 1fr;
 align-items: center;
+gap: 1em;
+margin: 4em;
+padding: 1em 4em;
+grid-column: 2/3;
+justify-content: center;
 border-radius:2em;
 background-image: linear-gradient(135deg,#26CBD6,#5552F3);
 max-width:32em;
 min-height: 20em;
+`;
+
+export const Txt = styled.input`
+    grid-row: 2/3;
+    grid-column:2/3;
+    border: 0;
+    line-height:3em;
+    border-radius:1em;
+`;
+
+export const Pass = styled.input`
+    grid-row: 3/4;
+    grid-column:2/3;
+    border: 0;
+    line-height:3em;
+    border-radius:1em;
+`;
+
+export const Btn = styled.button`
+    grid-column: 1/3;
+    justify-self: center;
+    padding: 0.5em 4em;
+    line-height: 2em;
+    border-radius: 2em;
+    border: 0;
 `;
